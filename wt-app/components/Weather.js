30bf2c56600c13d4b10f01140206269cd2737968
natthanon105 <97437746+natthanon105@users.noreply.github.com>
2022-08-17 09:27:33 +0700

    Part 4

--- wt-app/components/Weather.js
+++ wt-app/components/Weather.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {ImageBackground, Text, View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ImageBackground, Text, View, StyleSheet } from 'react-native';
 import Forecast from './Forcast';
 
 export default function Weather(props) {
@@ -8,6 +8,25 @@ export default function Weather(props) {
         description: 'description',
         temp: 0
     })
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=7d63bcc40ca31f4ddf07e5daa47633b6`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
+
     return (
         <View>
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
@@ -27,15 +46,15 @@ const styles = StyleSheet.create({
         height: '100%'
     },
 
-    contain:{
-        flexDirection:'column',
-        alignItems:'center',
+    contain: {
+        flexDirection: 'column',
+        alignItems: 'center',
         backgroundColor: 'rgba(52, 52, 52, 0.5)',
         width: '100%',
         padding: 30
     },
 
-    text:{
+    text: {
         color: 'white',
     },
 
